2748ee3882baf0b3d5a55fc61c7cf39d92650abd
SivaurbanJeyakaran <sivacsk2@gmail.com>
2025-12-08 09:31:40 +0530

    Add FLAMES game and documentation

---
+++ Basic.py
@@ -0,0 +1,4 @@
+dumbledore_army=("Harry","Ron","Hermoine","Ginny","fred","Gerorge","Neville","Luna","Dean","Cho")
+order_of_phenoix = ["Albus","Sirius","Lupin","Peter","Aruther","Snape"]
+new_order=dumbledore_army*2
+print(new_order)
